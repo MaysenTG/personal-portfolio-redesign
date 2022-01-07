18f41352e02b027718a897e8848e85648e91d98c
Maysen Greenwood <greenwood.maysen@gmail.com>
2022-01-07 14:22:11 +1300

    Add conditional settings to change layout of CV page slightly

--- src/PageComponents/cv.js
+++ src/PageComponents/cv.js
@@ -11,10 +11,20 @@ class MaysenCV extends Component {
       education_json: [],
       work_json: [],
       skills_json: [],
+      skills_languages_json: [],
+      skills_frameworks_json: [],
       hobbies_json: [],
 
-      firestoreJSONObj: ["education", "work", "skills", "hobbies"],
+      firestoreJSONObj: [
+        "education",
+        "work",
+        "skills/frameworks",
+        "skills/languages",
+        "hobbies",
+        "settings/cvPage",
+      ],
       loading: true,
+      cv_settings: { showHobbies: null, showWidth: null },
     };
   }
 
@@ -22,16 +32,29 @@ class MaysenCV extends Component {
     const db = getDatabase();
 
     this.state.firestoreJSONObj.map(async (jsonItem) => {
-      let stateName = jsonItem + "_json";
+      let stateName;
+      if (jsonItem === "skills/frameworks") {
+        stateName = "skills_frameworks_json";
+      } else if (jsonItem === "skills/languages") {
+        stateName = "skills_languages_json";
+      } else if (jsonItem === "settings/cvPage") {
+        stateName = "cv_settings";
+      } else {
+        stateName = jsonItem + "_json";
+      }
+
       const starCountRef = ref(db, jsonItem);
 
       await onValue(starCountRef, (snapshot) => {
         const data = snapshot.val();
-        this.setState({ [stateName]: data.map((item) => item) });
+        if (jsonItem === "settings/cvPage") {
+          this.setState({ [stateName]: data });
+        } else {
+          this.setState({ [stateName]: data.map((item) => item) });
+        }
+        this.setState({ loading: false });
       });
     });
-
-    this.setState({ loading: false });
   }
 
   createCV() {
@@ -87,73 +110,103 @@ class MaysenCV extends Component {
             </div>
 
             <div className="group">
+              <div className="heading">
+                <h2 className="text-center">Skills</h2>
+              </div>
               <div className="row">
                 <div className="col-md-6">
                   <div className="skills portfolio-info-card">
-                    <h2>Skills</h2>
-                    {this.state.skills_json.map((skill) => (
+                    <h2>Languages</h2>
+                    {this.state.skills_languages_json.map((skill) => (
                       <div key={`${skill.id}-skill`}>
-                        <h3>{skill.language}</h3>
-                        <div className="progress">
-                          <div
-                            className="progress-bar"
-                            aria-valuenow={skill.widthPercentage}
-                            aria-valuemin="0"
-                            aria-valuemax="100"
-                            style={{ width: `${skill.widthPercentage}%` }}
-                          >
-                            <span className="visually-hidden">
-                              {skill.widthPercentage}%
-                            </span>
+                        {this.state.cv_settings.showWidth ? (
+                          <div>
+                            <h3>{skill.name}</h3>
+                            <div className="progress">
+                              <div
+                                className="progress-bar"
+                                aria-valuenow={skill.widthPercentage}
+                                aria-valuemin="0"
+                                aria-valuemax="100"
+                                style={{ width: `${skill.widthPercentage}%` }}
+                              >
+                                <span className="visually-hidden">
+                                  {skill.widthPercentage}%
+                                </span>
+                              </div>
+                            </div>
                           </div>
-                        </div>
+                        ) : (
+                          <>
+                            <h3>
+                              <li>{skill.name}</li>
+                            </h3>
+                          </>
+                        )}
                       </div>
                     ))}
                   </div>
                 </div>
 
                 <div className="col-md-6">
-                  <div className="contact-info portfolio-info-card">
-                    <h2>Contact Info</h2>
-                    <div className="row">
-                      <div className="col-1">
-                        <i className="icon ion-person icon"></i>
-                      </div>
-                      <div className="col-9">
-                        <span>Maysen Greenwood</span>
-                      </div>
-                    </div>
-                    <div className="row">
-                      <div className="col-1">
-                        <i className="icon ion-ios-telephone icon"></i>
-                      </div>
-                      <div className="col-9">
-                        <span>+64 027 310 2660</span>
-                      </div>
-                    </div>
-                    <div className="row">
-                      <div className="col-1">
-                        <i className="icon ion-at icon"></i>
-                      </div>
-                      <div className="col-9">
-                        <span>greenwood.maysen@gmail.com</span>
-                      </div>
-                    </div>
+                  <div className="skills portfolio-info-card">
+                    <h2>Frameworks</h2>
+                    {this.state.skills_frameworks_json.map(
+                      (framework_skill) => (
+                        <div key={`${framework_skill.id}-skill`}>
+                          <div key={`${framework_skill.id}-skill`}>
+                            {this.state.cv_settings.showWidth ? (
+                              <div>
+                                <h3>{framework_skill.name}</h3>
+                                <div className="progress">
+                                  <div
+                                    className="progress-bar"
+                                    aria-valuenow={
+                                      framework_skill.widthPercentage
+                                    }
+                                    aria-valuemin="0"
+                                    aria-valuemax="100"
+                                    style={{
+                                      width: `${framework_skill.widthPercentage}%`,
+                                    }}
+                                  >
+                                    <span className="visually-hidden">
+                                      {framework_skill.widthPercentage}%
+                                    </span>
+                                  </div>
+                                </div>
+                              </div>
+                            ) : (
+                              <>
+                                <h3>
+                                  <li>{framework_skill.name}</li>
+                                </h3>
+                              </>
+                            )}
+                          </div>
+                        </div>
+                      )
+                    )}
                   </div>
                 </div>
               </div>
             </div>
-
-            {this.state.hobbies_json.map((item) => (
-              <div className="hobbies group" key={`${item.id}-hobbies`}>
-                <div className="heading">
-                  <h2 className="text-center">Hobbies</h2>
-                </div>
-                <p className="text-center text-muted">
-                  {parse(item.description)}
-                </p>
-              </div>
-            ))}
+            {this.state.cv_settings.showHobbies ? (
+              <>
+                {this.state.hobbies_json.map((item) => (
+                  <div className="hobbies group" key={`${item.id}-hobbies`}>
+                    <div className="heading">
+                      <h2 className="text-center">Hobbies</h2>
+                    </div>
+                    <p className="text-center text-muted">
+                      {parse(item.description)}
+                    </p>
+                  </div>
+                ))}
+              </>
+            ) : (
+              <></>
+            )}
           </div>
         </section>
       </main>
@@ -165,7 +218,9 @@ class MaysenCV extends Component {
       return (
         <div className="loading-container">
           <h1>Loading CV</h1>
-          <CircularProgress />
+          <span className="loading-item">
+            <CircularProgress />
+          </span>
         </div>
       );
     } else {
